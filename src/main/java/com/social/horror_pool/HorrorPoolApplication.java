c1ecdbdaa350393f73bfc6670a79fcfa8618c436
Ilya Ibragimov <ilya.ibragimov@seznam.cz>
2025-05-12 22:42:18 +0200

    initialize the project

---
+++ src/main/java/com/social/horror_pool/HorrorPoolApplication.java
@@ -0,0 +1,13 @@
+package com.social.horror_pool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorrorPoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HorrorPoolApplication.class, args);
+    }
+
+}
