52d26db41309eb24010c817c99de548ca719099b
Ilya Ibragimov <ilya.ibragimov@seznam.cz>
2025-05-14 21:22:38 +0200

    added GenreDTO

---
+++ src/main/java/com/social/horror_pool/dto/GenreDTO.java
@@ -0,0 +1,19 @@
+package com.social.horror_pool.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class GenreDTO {
+
+    private Long genreId;
+
+    @NotBlank(message = "Name cannot be blank")
+    @Size(min = 3, max = 30 , message = "Name must be 3-30 characters")
+    private String name;
+}
