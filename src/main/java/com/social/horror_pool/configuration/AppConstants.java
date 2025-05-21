88497c29f12ef6814f6993a36864b6e235c81523
Ilya Ibragimov <ilya.ibragimov@seznam.cz>
2025-05-21 21:01:25 +0200

    added AppConstants class for pagination

---
+++ src/main/java/com/social/horror_pool/configuration/AppConstants.java
@@ -0,0 +1,8 @@
+package com.social.horror_pool.configuration;
+
+public class AppConstants {
+
+    private final String PAGE_NUMBER = "0";
+    private final String PAGE_SIZE = "1";
+    private final String SORT_ORDER = "asc";
+}
