33b0a368d0c7eeec73434fac1f1ca232684170ee
Ilya Ibragimov <ilya.ibragimov@seznam.cz>
2025-05-27 22:04:36 +0200

    added UserRepository and RoleRepository

---
+++ src/main/java/com/social/horror_pool/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.social.horror_pool.repository;
+
+import com.social.horror_pool.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
