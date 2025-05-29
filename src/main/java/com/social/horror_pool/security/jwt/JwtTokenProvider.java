f74a8468db264235c0c6ff44520bea3f7789a20e
Ilya Ibragimov <ilya.ibragimov@seznam.cz>
2025-05-29 21:54:53 +0200

    added getUsernameFromToken and validateToken methods

--- src/main/java/com/social/horror_pool/security/jwt/JwtTokenProvider.java
+++ src/main/java/com/social/horror_pool/security/jwt/JwtTokenProvider.java
@@ -1,11 +1,15 @@
 package com.social.horror_pool.security.jwt;
 
-import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.security.Key;
 import java.util.Date;
 
@@ -18,10 +22,14 @@ public class JwtTokenProvider {
     @Value("${spring.app.jwtExpirationMs}")
     private int jwtExpirationMs;
 
+    private Key key;
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
 
     @PostConstruct
-    public Key initKey() {
-        return Keys.hmacShaKeyFor(jwtSecret.getBytes());
+    public void initKey() {
+        this.key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
     }
 
     public String generateTokenFromUsername(String username) {
@@ -32,11 +40,34 @@ public class JwtTokenProvider {
                 .subject(username)
                 .issuedAt(now)
                 .expiration(expiryDate)
-                .signWith(initKey())
+                .signWith(this.key)
                 .compact();
     }
 
+    public String getUsernameFromToken(String token) {
+        return Jwts.parser()
+                .verifyWith((SecretKey) this.key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload().getSubject();
+    }
 
-
-
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser()
+                    .verifyWith((SecretKey) key)
+                    .build()
+                    .parseSignedClaims(token);
+            return true;
+        } catch (SecurityException | MalformedJwtException ex) {
+            log.error("Invalid JWT signature.");
+        } catch (ExpiredJwtException ex) {
+            log.error("Expired JWT token.");
+        } catch (UnsupportedJwtException ex) {
+            log.error("Unsupported JWT token.");
+        } catch (IllegalArgumentException ex) {
+            log.error("Empty JWT token.");
+        }
+        return false;
+    }
 }
