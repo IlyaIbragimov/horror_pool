a515e9165b49de4f84ab1da5bbb35a94e2b3b8e1
Ilya Ibragimov <ilya.ibragimov@seznam.cz>
2025-05-29 23:32:34 +0200

    configurated SecurityConfig to use the JwtAuthenticationFilter

--- src/main/java/com/social/horror_pool/security/SecurityConfig.java
+++ src/main/java/com/social/horror_pool/security/SecurityConfig.java
@@ -1,25 +1,30 @@
 package com.social.horror_pool.security;
 
+import com.social.horror_pool.security.jwt.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     private final UserDetailsServiceImpl userDetailsServiceImpl;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
-    public SecurityConfig(final UserDetailsServiceImpl userDetailsServiceImpl) {
+
+    public SecurityConfig(final UserDetailsServiceImpl userDetailsServiceImpl, JwtAuthenticationFilter jwtAuthenticationFilter) {
         this.userDetailsServiceImpl = userDetailsServiceImpl;
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
     }
 
     @Bean
@@ -46,17 +51,15 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/horrorpool/signup","/horrorpool/signin", "/horrorpool/signout", "/login").permitAll()
+                        .requestMatchers("/horrorpool/signup","/horrorpool/signin", "/horrorpool/signout").permitAll()
                         .anyRequest().authenticated()
                 )
-                .formLogin(Customizer.withDefaults());
-
-        http.authenticationProvider(authenticationProvider());
+                .sessionManagement(session ->
+                                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authenticationProvider(authenticationProvider())
+                .addFilterBefore(this.jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
-
-
-
-
 }
