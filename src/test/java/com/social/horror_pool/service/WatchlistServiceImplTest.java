1e91ac0c13ceee2f8a690484e0c5c35b2b13eee1
Ilya Ibragimov <ilya.ibragimov@seznam.cz>
2025-08-06 22:30:12 +0200

    added updateWatchlist_NotAuthorisedUser_ReturnAPIException() unit test

--- src/test/java/com/social/horror_pool/service/WatchlistServiceImplTest.java
+++ src/test/java/com/social/horror_pool/service/WatchlistServiceImplTest.java
@@ -130,6 +130,16 @@ public class WatchlistServiceImplTest {
         assertEquals("Watchlist was not found with id : 4", exception.getMessage());
     }
 
+    @Test
+    public void updateWatchlist_NotAuthorisedUser_ReturnAPIException() {
+        when(watchlistRepository.findById(1L)).thenReturn(Optional.of(watchlist1));
+        when(userRepository.findByUsername("username1")).thenReturn(Optional.of(user2));
+
+        APIException exception = assertThrows(APIException.class, () -> watchlistServiceImpl.updateWatchlist(1L, "Updated"));
+
+        assertEquals("You do not have permission to modify this watchlist.", exception.getMessage());
+    }
+
     private Watchlist createWatchlist(Long watchlistId, String title, User user) {
         Watchlist watchlist = new Watchlist();
         watchlist.setWatchlistId(watchlistId);
