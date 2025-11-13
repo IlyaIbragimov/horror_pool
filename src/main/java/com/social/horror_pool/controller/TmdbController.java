c0e012caf5f864b56d11f409c02c7d75cccdfbfc
Ilya Ibragimov <ilya.ibragimov@seznam.cz>
2025-11-13 22:04:11 +0100

    added TmdbController

---
+++ src/main/java/com/social/horror_pool/controller/TmdbController.java
@@ -0,0 +1,26 @@
+package com.social.horror_pool.controller;
+
+import com.social.horror_pool.dto.MovieDTO;
+import com.social.horror_pool.service.MovieService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/horrorpool/public/tmdb")
+public class TmdbController {
+
+    private final MovieService movieService;
+
+    public TmdbController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @PostMapping("/import/{tmdbId}")
+    public ResponseEntity<MovieDTO> importMovie(
+            @PathVariable Long tmdbId,
+            @RequestParam(defaultValue = "en-US") String language) {
+        return this.movieService.importFromTmdb(tmdbId, language)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+}
