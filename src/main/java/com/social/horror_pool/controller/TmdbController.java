77e6db8a3dd93b0e059be3228bc97aef4a11b402
Ilya Ibragimov <ilya.ibragimov@seznam.cz>
2025-12-04 00:44:25 +0100

    changed endpoint from TmdbController to admin

--- src/main/java/com/social/horror_pool/controller/TmdbController.java
+++ src/main/java/com/social/horror_pool/controller/TmdbController.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/horrorpool/public/tmdb")
+@RequestMapping("/horrorpool/admin/tmdb")
 public class TmdbController {
 
     private final MovieService movieService;
