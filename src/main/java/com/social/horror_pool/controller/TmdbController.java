e86cdd2cb32c55f5da654a92ec5739e012c3eb39
Ilya Ibragimov <ilya.ibragimov@seznam.cz>
2025-12-19 19:39:21 +0100

    added bulkImportFromTmdb POST method to TmdbController

--- src/main/java/com/social/horror_pool/controller/TmdbController.java
+++ src/main/java/com/social/horror_pool/controller/TmdbController.java
@@ -1,6 +1,7 @@
 package com.social.horror_pool.controller;
 
 import com.social.horror_pool.dto.MovieDTO;
+import com.social.horror_pool.payload.tmdb.BulkImportResultResponse;
 import com.social.horror_pool.service.MovieService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,10 +18,18 @@ public class TmdbController {
     }
 
     @PostMapping("/import/{tmdbId}")
-    public ResponseEntity<MovieDTO> importMovie(
+    public ResponseEntity<MovieDTO> importMovieFromTmdb(
             @PathVariable Long tmdbId,
             @RequestParam(defaultValue = "en-US") String language) {
         MovieDTO result = this.movieService.importFromTmdb(tmdbId, language);
         return new ResponseEntity<MovieDTO>(result, HttpStatus.OK);
+    }
+
+    @PostMapping("/bulkImport")
+    public ResponseEntity<BulkImportResultResponse> bulkImportFromTmd(
+            @RequestParam(defaultValue = "1") Integer pages,
+            @RequestParam(defaultValue = "en-US") String language) {
+        BulkImportResultResponse result = this.movieService.bulkImportFromTmdb(pages, language);
+        return new ResponseEntity<BulkImportResultResponse>(result, HttpStatus.OK);
     }
 }
