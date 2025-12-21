8cd53f05873d6a886c7caa1fce31b58471011a66
Ilya Ibragimov <ilya.ibragimov@seznam.cz>
2025-12-21 19:15:35 +0100

    added swagger description for endpoints from TmdbController

--- src/main/java/com/social/horror_pool/controller/TmdbController.java
+++ src/main/java/com/social/horror_pool/controller/TmdbController.java
@@ -3,10 +3,13 @@ package com.social.horror_pool.controller;
 import com.social.horror_pool.dto.MovieDTO;
 import com.social.horror_pool.payload.tmdb.BulkImportResultResponse;
 import com.social.horror_pool.service.MovieService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "TMDB", description = "Endpoints for movie import from tmdb")
 @RestController
 @RequestMapping("/horrorpool/admin/tmdb")
 public class TmdbController {
@@ -17,6 +20,10 @@ public class TmdbController {
         this.movieService = movieService;
     }
 
+    @Operation(
+            summary = "Import one movie from TMDB by tmdbId",
+            description = "Import one movie from TMDB by tmdbId to the database. Available only for administrator"
+    )
     @PostMapping("/import/{tmdbId}")
     public ResponseEntity<MovieDTO> importMovieFromTmdb(
             @PathVariable Long tmdbId,
@@ -25,6 +32,10 @@ public class TmdbController {
         return new ResponseEntity<MovieDTO>(result, HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Bulk import from TMDB",
+            description = "Import given amount movies from TMDB (20 movies per one page). Available only for administrator"
+    )
     @PostMapping("/bulkImport")
     public ResponseEntity<BulkImportResultResponse> bulkImportFromTmd(
             @RequestParam(defaultValue = "1") Integer pages,
