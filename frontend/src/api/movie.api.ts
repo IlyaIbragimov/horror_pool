6387fa5633f31bc24d2e2ecbb8b3d8af598d35f5
Ilya Ibragimov <ilya.ibragimov@seznam.cz>
2026-01-01 17:56:21 +0100

    removed comments from movie.api.ts

--- frontend/src/api/movie.api.ts
+++ frontend/src/api/movie.api.ts
@@ -1,36 +1,3 @@
-// import { http } from "./http";
-// import type { MovieAllResponse, MovieDTOLight } from "../types/movie.types";
-
-// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL as string;
-
-// export async function fetchMovies(params?: {
-//   page?: number;
-//   size?: number;
-//   sort?: string;
-//   order?: string;
-// }): Promise<MovieAllResponse> {
-//   const search = new URLSearchParams();
-
-//   if (params?.page !== undefined) search.set("page", String(params.page));
-//   if (params?.size !== undefined) search.set("size", String(params.size));
-//   if (params?.sort) search.set("sort", params.sort);
-//   if (params?.order) search.set("order", params.order);
-
-//   const url = `${API_BASE_URL}/public/movie/all?${search.toString()}`;
-
-//   const res = await fetch(url, {
-//     method: "GET",
-//     headers: { Accept: "application/json" },
-//   });
-
-//   if (!res.ok) {
-//     const text = await res.text().catch(() => "");
-//     throw new Error(`Failed to fetch movies: ${res.status} ${res.statusText} ${text}`);
-//   }
-
-//   return res.json();
-// }
-
 import { http } from "./http";
 import type { MovieAllResponse } from "../types/movie.types";
 
