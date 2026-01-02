2d137479657c2b8964152facaa494b4e1267a4bc
Ilya Ibragimov <ilya.ibragimov@seznam.cz>
2026-01-02 23:46:57 +0100

    added AppLayout.tsx to apply global page styles for all pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,16 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { MoviesPage } from "./pages/MoviesPage/MoviesPage";
 import { MoviePage } from "./pages/MoviePage/MoviePage";
+import { AppLayout } from "./components/AppLayout.tsx";
 
 export function App() {
   return (
+    <AppLayout>
     <Routes>
       <Route path="/" element={<Navigate to="/movies" replace />} />
       <Route path="/movies" element={<MoviesPage />} />
       <Route path="/movies/:movieId" element={<MoviePage />} />
     </Routes>
+    </AppLayout>
   );
 }
