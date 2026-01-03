c9a078431e64c573b9caf95e703e58d070cd41ad
Ilya Ibragimov <ilya.ibragimov@seznam.cz>
2026-01-03 18:56:17 +0100

    added Footer.tsx and Footer.css . Added Footer to AppLayout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,16 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import { MoviesPage } from "./pages/MoviesPage/MoviesPage";
 import { MoviePage } from "./pages/MoviePage/MoviePage";
 import { AppLayout } from "./components/AppLayout.tsx";
+import './App.css'
 
 export function App() {
   return (
-    <AppLayout>
     <Routes>
-      <Route path="/" element={<Navigate to="/movies" replace />} />
-      <Route path="/movies" element={<MoviesPage />} />
-      <Route path="/movies/:movieId" element={<MoviePage />} />
+      <Route element={<AppLayout />}>
+       <Route path="/" element={<Navigate to="/movies" replace />} />
+       <Route path="/movies" element={<MoviesPage />} />
+       <Route path="/movies/:movieId" element={<MoviePage />} />
+      </Route>
     </Routes>
-    </AppLayout>
   );
 }
