67e8e0989b4405310227b355facdea7fe089e6fd
Ilya Ibragimov <ilya.ibragimov@seznam.cz>
2026-02-05 00:56:42 +0100

    added SignInPage and SignUpPage as modal routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,37 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
 import { MoviesPage } from "./pages/MoviesPage/MoviesPage";
 import { MoviePage } from "./pages/MoviePage/MoviePage";
 import { GenresPage } from "./pages/GenresPage/GenresPage";
 import SignUpPage from "./pages/SignUpPage/SignUpPage.tsx";
-import SignInPage from "./pages/SignInPage/SignInPage"
+import SignInPage from "./pages/SignInPage/SignInPage";
 import { AppLayout } from "./components/AppLayout.tsx";
-import './App.css'
+import "./App.css";
 
 export function App() {
+  const location = useLocation();
+  const state = location.state as { backgroundLocation?: Location } | null;
+  const backgroundLocation = state?.backgroundLocation;
+
   return (
-    <Routes>
-      <Route element={<AppLayout />}>
-       <Route path="/" element={<Navigate to="/movies" replace />} />
-       <Route path="/movies" element={<MoviesPage />} />
-       <Route path="/movies/:movieId" element={<MoviePage />} />
-       <Route path="/genres" element={<GenresPage />} />
-       <Route path="/login" element={ <SignInPage/>}/>
-       <Route path="/register" element={ <SignUpPage/>}/>
-      </Route>
-    </Routes>
+    <>
+      <Routes location={backgroundLocation || location}>
+        <Route element={<AppLayout />}>
+          <Route path="/" element={<Navigate to="/movies" replace />} />
+          <Route path="/movies" element={<MoviesPage />} />
+          <Route path="/movies/:movieId" element={<MoviePage />} />
+          <Route path="/genres" element={<GenresPage />} />
+        </Route>
+
+        <Route path="/login" element={<SignInPage />} />
+        <Route path="/register" element={<SignUpPage />} />
+      </Routes>
+
+      {backgroundLocation && (
+        <Routes>
+          <Route path="/login" element={<SignInPage />} />
+          <Route path="/register" element={<SignUpPage />} />
+        </Routes>
+      )}
+    </>
   );
 }
