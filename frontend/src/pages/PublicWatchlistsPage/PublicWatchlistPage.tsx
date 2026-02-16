8e814e167736b0eed999575398d17a0b48cbe662
Ilya Ibragimov <ilya.ibragimov@seznam.cz>
2026-02-16 23:37:47 +0100

    created PublicWatchlistPage.tsx and styles in PublicWatchlistPage.module.css

---
+++ frontend/src/pages/PublicWatchlistsPage/PublicWatchlistPage.tsx
@@ -0,0 +1,66 @@
+import { useEffect, useState } from "react";
+import { getAllPublicWatchlists } from "../../api/watchlist.api";
+import type { WatchlistAllResponse } from "../../types/watchlist.types";
+import { WatchlistCard } from "../../components/WatchlistCard/WatchlistCard";
+import styles from "./PublicWatchlistPage.module.css";
+import { Link } from "react-router-dom";
+
+export function PublicWatchlistPage() {
+  const [page, setPage] = useState(1);
+  const [size] = useState(18);
+
+  const [data, setData] = useState<WatchlistAllResponse | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    setLoading(true);
+    setError(null);
+
+    getAllPublicWatchlists({ page: page - 1, size, order: "asc", sort: "name" })
+      .then(setData)
+      .catch((e) => setError(e instanceof Error ? e.message : String(e)))
+      .finally(() => setLoading(false));
+  }, [page, size]);
+
+  return (
+    <div className={styles.page}>
+      <div className={styles.header}>
+        <div className={styles.pager}>
+          <button
+            disabled={loading || page <= 1}
+            onClick={() => setPage((p) => p - 1)}
+          >
+            Prev
+          </button>
+
+          <span>
+            Page {data ? data.pageNumber + 1 : page} / {data?.totalPages ?? "?"}
+          </span>
+
+          <button
+            disabled={loading || !data || data.lastPage}
+            onClick={() => setPage((p) => p + 1)}
+          >
+            Next
+          </button>
+        </div>
+      </div>
+
+      {error && <div className={styles.error}>{error}</div>}
+      {loading && <div>Loading...</div>}
+
+      <div className={styles.list}>
+        {data?.watchlistDTOS.map((w) => (
+          <Link
+            key={w.watchlistId}
+            to={`/watchlist/${w.watchlistId}`}
+            className={styles.cardLink}
+          >
+            <WatchlistCard key={w.watchlistId} watchlist={w} />
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
