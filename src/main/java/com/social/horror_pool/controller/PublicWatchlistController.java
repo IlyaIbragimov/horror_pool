a8ef07c7f320853d8a5912f255e647257efa5885
Ilya Ibragimov <ilya.ibragimov@seznam.cz>
2026-02-19 01:36:33 +0100

    renamed WatchlistByIdResponse to WatchlistItemsByWatchlistIdResponse and small fix to getWatchlistItemsByWatchlistId method in WatchlistServiceImpl.java

--- src/main/java/com/social/horror_pool/controller/PublicWatchlistController.java
+++ src/main/java/com/social/horror_pool/controller/PublicWatchlistController.java
@@ -2,7 +2,7 @@ package com.social.horror_pool.controller;
 
 import com.social.horror_pool.configuration.AppConstants;
 import com.social.horror_pool.payload.WatchlistAllResponse;
-import com.social.horror_pool.payload.WatchlistByIdResponse;
+import com.social.horror_pool.payload.WatchlistItemsByWatchlistIdResponse;
 import com.social.horror_pool.service.WatchlistService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -42,14 +42,14 @@ public class PublicWatchlistController {
             description = "Retrieve movies of a watchlist by ID with optional filtering by watched status and pagination. Only accessible to user who created the watchlist if watchlist is private, in case if it is public available for all."
     )
     @GetMapping("/{watchlistId}")
-    public ResponseEntity<WatchlistByIdResponse> getWatchlistItemsByWatchlistId(
+    public ResponseEntity<WatchlistItemsByWatchlistIdResponse> getWatchlistItemsByWatchlistId(
             @PathVariable Long watchlistId,
             @RequestParam(name = "watched", required = false) Boolean watched,
             @RequestParam(name = "page", defaultValue = AppConstants.PAGE_NUMBER, required = false)  Integer pageNumber,
             @RequestParam(name = "size", defaultValue = AppConstants.PAGE_SIZE, required = false)  Integer pageSize,
             @RequestParam(name = "order", defaultValue = AppConstants.ORDER_TYPE, required = false) String order
     ){
-        WatchlistByIdResponse response = this.watchlistService.getWatchlistItemsByWatchlistId(watchlistId, watched, pageNumber, pageSize, order);
+        WatchlistItemsByWatchlistIdResponse response = this.watchlistService.getWatchlistItemsByWatchlistId(watchlistId, watched, pageNumber, pageSize, order);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
