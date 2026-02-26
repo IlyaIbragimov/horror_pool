f3a4c564d9f3764f0ac4251226905cc3090cfe98
Ilya Ibragimov <ilya.ibragimov@seznam.cz>
2026-02-26 21:22:44 +0100

    added UserWatchlistPage as Route to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { MoviesPage } from "./pages/MoviesPage/MoviesPage";
 import { MoviePage } from "./pages/MoviePage/MoviePage";
 import { GenresPage } from "./pages/GenresPage/GenresPage";
 import { PublicWatchlistPage } from "./pages/PublicWatchlistsPage/PublicWatchlistPage.tsx";
+import { UserWatchlistPage } from "./pages/UserPage/UserWatchlistPage.tsx";
 import { WatchlistPage } from "./pages/WatchlistPage/WatchlistPage.tsx";
 import SignUpPage from "./pages/SignUpPage/SignUpPage.tsx";
 import SignInPage from "./pages/SignInPage/SignInPage";
@@ -24,6 +25,7 @@ export function App() {
           <Route path="/genres" element={<GenresPage />} />
           <Route path="/watchlistPublic" element={<PublicWatchlistPage />} />
           <Route path="/watchlist/:watchlistId" element={<WatchlistPage />} />
+          <Route path="/watchlistUser" element={<UserWatchlistPage />} />
         </Route>
 
         <Route path="/login" element={<SignInPage />} />
