1c04ae27016f2843a215ddc22b7f0fa5ceaaca9d
Ilya Ibragimov <ilya.ibragimov@seznam.cz>
2026-04-22 00:06:39 +0200

    update bulkImportFromTmdb endpoint

--- src/main/java/com/social/horror_pool/controller/TmdbController.java
+++ src/main/java/com/social/horror_pool/controller/TmdbController.java
@@ -2,11 +2,10 @@ package com.social.horror_pool.controller;
 
 import com.social.horror_pool.dto.MovieDTO;
 import com.social.horror_pool.payload.tmdb.BulkImportResultResponse;
+import com.social.horror_pool.payload.tmdb.TmdbDiscoverRequest;
 import com.social.horror_pool.service.MovieService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -40,9 +39,8 @@ public class TmdbController {
     )
     @PostMapping("/bulkImport")
     public ResponseEntity<BulkImportResultResponse> bulkImportFromTmd(
-            @RequestParam(name = "pages", defaultValue = "1") @Min(1) @Max(10) Integer pages,
-            @RequestParam(name = "language", defaultValue = "en-US") String language) {
-        BulkImportResultResponse result = this.movieService.bulkImportFromTmdb(pages, language);
+            @RequestBody(required = false) TmdbDiscoverRequest request) {
+        BulkImportResultResponse result = this.movieService.bulkImportFromTmdb(request);
         return new ResponseEntity<BulkImportResultResponse>(result, HttpStatus.OK);
     }
 }
