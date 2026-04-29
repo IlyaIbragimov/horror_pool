4cc5ae3d560b19737fa887b1736a36918e327407
Ilya Ibragimov <ilya.ibragimov@seznam.cz>
2026-04-29 23:15:08 +0200

    remove description, backdropPath , adult and video fields

--- src/main/java/com/social/horror_pool/dto/MovieDTO.java
+++ src/main/java/com/social/horror_pool/dto/MovieDTO.java
@@ -1,7 +1,6 @@
 package com.social.horror_pool.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,21 +20,16 @@ public class MovieDTO {
     @NotBlank(message = "Movie title cannot be empty")
     private String title;
     private String originalTitle;
-    @Size(max = 1000, message = "Description cannot be longer than 1000 characters")
-    private String description;
     private String overview;
     private LocalDate releaseDate;
     private Integer releaseYear;
     private String posterPath;
-    private String backdropPath;
 
     private Double voteAverage;
     private Integer voteCount;
     private Double popularity;
 
     private String originalLanguage;
-    private Boolean adult;
-    private Boolean video;
 
     private List<GenreDTO> genres = new ArrayList<>();
 
