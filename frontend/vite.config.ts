016219d9d397b81e274cedec479dbed4e773a3d5
Ilya Ibragimov <ilya.ibragimov@seznam.cz>
2026-06-18 00:42:24 +0200

    made BASE_URL parameter configurable through variable VITE_DEV_BACKEND_URL

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+const backendUrl = process.env.VITE_DEV_BACKEND_URL ?? 'http://localhost:8080';
 
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/horrorpool' :{
-        target: 
-        'http://localhost:8080',
+      '/horrorpool': {
+        target: backendUrl,
         changeOrigin: true,
-        secure: false
-      }
-    }
-  }
-})
+        secure: false,
+      },
+    },
+  },
+});
