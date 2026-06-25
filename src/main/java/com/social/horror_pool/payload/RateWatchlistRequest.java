e86c1d01e173a15197156d4294260615f5a34cb1
Ilya Ibragimov <ilya.ibragimov@seznam.cz>
2026-06-25 23:35:23 +0200

    Added explicit validation messages. Changed rating to Double

--- src/main/java/com/social/horror_pool/payload/RateWatchlistRequest.java
+++ src/main/java/com/social/horror_pool/payload/RateWatchlistRequest.java
@@ -1,14 +1,14 @@
 package com.social.horror_pool.payload;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class RateWatchlistRequest {
-    @Min(0)
-    @Max(10)
-    @NotNull
-    private double rating;
+    @NotNull(message = "Rating is required")
+    @DecimalMin(value = "0.0", message = "Rating must be at least 0")
+    @DecimalMax(value = "10.0", message = "Rating must not exceed 10")
+    private Double rating;
 }
