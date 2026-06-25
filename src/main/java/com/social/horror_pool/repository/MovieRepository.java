320fbf42a13fa63516a45b30eb7c6ce09d841894
Ilya Ibragimov <ilya.ibragimov@seznam.cz>
2026-06-25 23:08:18 +0200

    Removed findByTitle added existsByTitleIgnoreCaseAndReleaseDate and existsByTitleIgnoreCaseAndReleaseDateAndMovieIdNot

--- src/main/java/com/social/horror_pool/repository/MovieRepository.java
+++ src/main/java/com/social/horror_pool/repository/MovieRepository.java
@@ -1,16 +1,17 @@
 package com.social.horror_pool.repository;
 
 import com.social.horror_pool.model.Movie;
-import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 public interface MovieRepository extends JpaRepository<Movie, Long>, JpaSpecificationExecutor<Movie> {
-    Movie findByTitle(@NotBlank(message = "Movie title cannot be empty") String title);
+    boolean existsByTitleIgnoreCaseAndReleaseDate(String title, LocalDate releaseDate);
+    boolean existsByTitleIgnoreCaseAndReleaseDateAndMovieIdNot(String title, LocalDate releaseDate, Long movieId);
     Optional<Movie> findByTmdbId(Long tmdbId);
     boolean existsByTmdbId(Long tmdbId);
     Page<Movie> findByGenres_GenreId(Long genreId, Pageable pageable);
